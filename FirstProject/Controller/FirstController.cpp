afceed1c82c48728cb7b2334f59d3878f9b576a9
snoupyrelete <drob8896@740s-pl-112.local>
2017-01-26 07:25:46 -0700

    Added special output private method

--- FirstProject/Controller/FirstController.cpp
+++ FirstProject/Controller/FirstController.cpp
@@ -10,11 +10,37 @@
 #include <iostream>
 #include "FirstController.h"
 
+
 using namespace std;
 
 // Angle brackets point to console out - where the stream goes.
 // Tells FirstController that start() belongs to it with the :: syntax
+
 void FirstController :: start()
 {
     cout << "words here!" << endl;
+    int myAge = 18;
+    cout << "I am " << myAge << " years old. #swag" << endl;
+    cout << "\n";
+    
+    this->specialOutput();
+}
+
+void FirstController :: specialOutput()
+{
+    int yourAge;
+    cout << "How old are you?" << endl;
+    cin >> yourAge;
+    cout << "You are " << yourAge << " years old." << endl;
+    
+    cin.ignore();
+    
+    string yourName;
+    cout << "type in your name" << endl;
+    getline(cin, yourName);
+    cout << "your name is " << yourName << endl;
+    
+    cout << "Type in your name and age" << endl;
+    cin >> yourName >> yourAge;
+    cout << "you typed: " << yourName << " and " << yourAge << endl;
 }
